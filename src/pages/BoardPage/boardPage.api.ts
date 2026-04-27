406854ceeea14a2e6386d735f504c161f9d01eff
lucasfloripa <lucas.floripa@icloud.com>
2026-04-27 20:29:50 -0300

    fix: refreshToken

--- src/pages/BoardPage/boardPage.api.ts
+++ src/pages/BoardPage/boardPage.api.ts
@@ -6,7 +6,8 @@ import {
 
 export const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL ?? 'http://localhost:4000',
-  timeout: 15000
+  timeout: 15000,
+  withCredentials: true
 })
 
 api.interceptors.request.use((config) => {
